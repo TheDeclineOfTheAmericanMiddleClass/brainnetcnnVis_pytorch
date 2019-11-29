841705d8ee427ef809632a11ecac6f6ad53c767c
Johann Kruschwitz <johann.kruschwitz@charite.de>
2019-11-29 23:08:32 +0100

    added assertion for positive definiteness in partial correlation matrices, reading data in as np.array now

--- read_data.py
+++ read_data.py
@@ -1,10 +1,10 @@
+from __future__ import print_function
 import h5py
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
 import torch
 import numpy as np
-from __future__ import print_function
 
 ################# Reading in the data ##################
 ########################################################
@@ -13,7 +13,8 @@ from __future__ import print_function
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # setting data directory
-dataDir = '3T_HCP1200_MSMAll_d300_ts2_RIDGE'
+dataDir = '3T_HCP1200_MSMAll_d300_ts2_RIDGE'  # ICA FIX 300 from ?HCP/LEA
+# dataDir = 'POWER_264_FIXEXTENDED_RIDGEP'
 
 # Names of only the files in the folder
 filenames = [f for f in listdir(dataDir) if isfile(join(dataDir, f))]
@@ -29,16 +30,19 @@ data = []
 # Reading in data from all 1003 subjects
 for i, filename in enumerate(filenames):
     hf = h5py.File(f'{dataDir}/{filename}', 'r')
-    # n1 = torch.tensor(hf["CorrMatrix"][:])
-    n1 = torch.tensor(hf["CorrMatrix"][:], device=device)
+    # n1 = torch.tensor(hf["CorrMatrix"][:], device=device) # maybe just push to tensor after all tangent space transforms complete
+    n1 = np.array(hf["CorrMatrix"][:])
     data.append(n1)
 
+# Data as a numpy array
+data = np.array(data)
+
 # Demographic data
-Bdata = pd.read_csv('hcp/behavioral.csv')
-Rdata = pd.read_csv('hcp/restricted.csv')
+behavioral = pd.read_csv('hcp/behavioral.csv')
+restricted = pd.read_csv('hcp/restricted.csv')
 
 # Specifying indices of overlapping subjects in 1206 and 1003 subject datasets
-subInd = np.where(np.isin(Rdata["Subject"], subnums))[0]
+subInd = np.where(np.isin(restricted["Subject"], subnums))[0]
 
 # Only using data from 1003
-Rdata = Rdata.reindex(subInd)
+restricted = restricted.reindex(subInd)
